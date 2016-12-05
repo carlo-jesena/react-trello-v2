169f820852f04ebb92ef90700c0650bcddb497de
Your Namecarlo <carlo.jesena@gmail.com>
2016-12-05 10:37:22 -0800

    added the rest of the files from previous projects

---
+++ js/components/card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Card() {
+	return (
+		<div className= "card">"This is a card Carlo"</div>
+	);
+}
---
+++ js/components/list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './card';
+
+export default function List() {
+	return (
+		<div className= "list">
+			<Card/>
+			<Card/>
+			<Card/>
+		</div>
+	);
+}
