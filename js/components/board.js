34ae01cdf08b125a62ce580ceefccccc9ac15eb8
Your Namecarlo <carlo.jesena@gmail.com>
2016-12-05 10:34:51 -0800

    initital commit

---
+++ js/components/board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './list';
+
+export default function Board() {
+	return (
+		<div classname = "board">
+			<List/>
+			<List/>
+			<List/>
+		</div>
+	);
+}
